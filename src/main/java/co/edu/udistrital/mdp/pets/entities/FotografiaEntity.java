cc60b5112806af1443e7c5b35c664095b9fb202f
Samuel Acosta <slacostac@udistrital.edu.co>
2026-09-06 13:11:17 -0500

    Remove unused JPA imports from entities
    Clean up unused jakarta.persistence imports in EventoVidaEntity and FotografiaEntity. Removed GeneratedValue, GenerationType, Id and MappedSuperclass imports to eliminate unused-import warnings and simplify entity classes.

--- src/main/java/co/edu/udistrital/mdp/pets/entities/FotografiaEntity.java
+++ src/main/java/co/edu/udistrital/mdp/pets/entities/FotografiaEntity.java
@@ -1,12 +1,7 @@
 package co.edu.udistrital.mdp.pets.entities;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.MappedSuperclass;
-
 import lombok.Data;
 import uk.co.jemos.podam.common.PodamExclude;
 
