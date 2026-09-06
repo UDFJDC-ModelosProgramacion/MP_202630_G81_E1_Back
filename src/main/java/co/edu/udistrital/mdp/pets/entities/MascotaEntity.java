ab07bf09b082714339441dd1b002170ea8faf42d
SANTIAGO-CAPERA <esrondonc@udistrital.edu.co>
2026-09-05 19:47:06 -0500

    Renombrar paquete ZZZ a pets y agregar placeholders temporales de MascotaEntity y AdoptanteEntity

---
+++ src/main/java/co/edu/udistrital/mdp/pets/entities/MascotaEntity.java
@@ -0,0 +1,13 @@
+package co.edu.udistrital.mdp.pets.entities;
+
+import lombok.Data;
+
+/**
+ * CLASE TEMPORAL / PROVISIONAL.
+ * Placeholder para permitir la compilacion local mientras el companero
+ * responsable sube la implementacion real de MascotaEntity.
+ * NO debe hacerse push de este archivo al repositorio compartido.
+ */
+@Data
+public class MascotaEntity extends BaseEntity {
+}
