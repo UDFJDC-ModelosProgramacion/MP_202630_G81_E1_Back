93a704ead93de5b0aa4217a2ac6514377170a92c
Samuel Acosta <slacostac@udistrital.edu.co>
2026-09-06 13:25:51 -0500

    Remove unused JPA imports from HistoriaExitoEntity
    Clean up unused Jakarta Persistence imports in HistoriaExitoEntity.java by removing Id, GeneratedValue, GenerationType, ManyToOne and MappedSuperclass. Keeps only the necessary imports (Entity, Temporal, TemporalType) and lombok.Data. No functional changes.

--- src/main/java/co/edu/udistrital/mdp/pets/entities/HistoriaExitoEntity.java
+++ src/main/java/co/edu/udistrital/mdp/pets/entities/HistoriaExitoEntity.java
@@ -3,11 +3,7 @@ package co.edu.udistrital.mdp.pets.entities;
 import java.sql.Date;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.MappedSuperclass;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
 import lombok.Data;
